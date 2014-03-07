8723bebc61d62b5b15da0179802c1f53a0284ad1
Bob Ball <bob.ball@citrix.com>
2014-03-07 17:20:45 +0000

    Add crontab to upload the CI status

--- inp/osci_installscript.sh
+++ inp/osci_installscript.sh
@@ -57,3 +57,8 @@ sudo tee -a /root/.ssh/known_hosts << KNOWN_HOST
 |1|uvH7eZ2XTbkdHUUXZ3XBScp6SO0=|uYehq2EeLpZYm2+UJew0YUhzUSU= ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDtdLzDzG6qmejiZq5BxDqxkN71W08xuQWVZ+6784SpsXTUujKT49lNCXmH+IHijsRaigU9cVFkWErVez0Q+NtUe077c5s50zCrL7EwH5/aiwaYklHF566TO7ctOJBLLsoVOUlJGpUAjM4veG9XMz0KhTP9qYK3zqNOcPV++551bQu1rc3kR8R8C/etmP60zMhVkUAdgyPWFZbmKlrBv1SxIpvjSo5STZzSRS7DK5/D9BaWS3zOcl5Pqtv0FVjm83dmQJxMPEjFo8e0T4Gq/noxYafQse4811/Ucmxj8J5rlJchakfxJz827w3MWYR4Ku+X3QAy/deBuvzUn3z35Zwr
 |1|v64yXgSHd9wX62/OTnmu4O91rXo=|NkrM8t/1ZlyLl0NhSXWx8GkvjcU= ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDtdLzDzG6qmejiZq5BxDqxkN71W08xuQWVZ+6784SpsXTUujKT49lNCXmH+IHijsRaigU9cVFkWErVez0Q+NtUe077c5s50zCrL7EwH5/aiwaYklHF566TO7ctOJBLLsoVOUlJGpUAjM4veG9XMz0KhTP9qYK3zqNOcPV++551bQu1rc3kR8R8C/etmP60zMhVkUAdgyPWFZbmKlrBv1SxIpvjSo5STZzSRS7DK5/D9BaWS3zOcl5Pqtv0FVjm83dmQJxMPEjFo8e0T4Gq/noxYafQse4811/Ucmxj8J5rlJchakfxJz827w3MWYR4Ku+X3QAy/deBuvzUn3z35Zwr
 KNOWN_HOST
+
+# Add uploading of status to crontab
+crontab - <<EOF
+*/10 * * * * /root/src/openstack-citrix-ci/upload_ci_status.sh
+EOF
