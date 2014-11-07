c064be7351e34ebaec303cc23fa19dae778dba3f
Mate Lakat <mate.lakat@citrix.com>
2014-11-07 10:43:10 +0100

    Renames - add _DIR suffix
    Signed-off-by: Mate Lakat <mate.lakat@citrix.com>

--- inp/installscript.sh
+++ inp/installscript.sh
@@ -4,11 +4,11 @@ NODEPOOL_REPO="$1"
 NODEPOOL_BRANCH="$2"
 
 NODEPOOL_USER="nodepool"
-NODEPOOL_HOME="/home/$NODEPOOL_USER"
-NODEPOOL_VENV="$NODEPOOL_HOME/env"
-NODEPOOL_SRC="$NODEPOOL_HOME/src/nodepool"
-NODEPOOL_CFG_DIR="$NODEPOOL_HOME/conf"
-NODEPOOL_LOGS="$NODEPOOL_HOME/logs"
+NODEPOOL_HOME_DIR="/home/$NODEPOOL_USER"
+NODEPOOL_VENV_DIR="$NODEPOOL_HOME_DIR/env"
+NODEPOOL_SRC_DIR="$NODEPOOL_HOME_DIR/src/nodepool"
+NODEPOOL_CFG_DIR="$NODEPOOL_HOME_DIR/conf"
+NODEPPOL_LOGS_DIR="$NODEPOOL_HOME_DIR/logs"
 
 
 ######
@@ -23,7 +23,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -qy \
 ######
 # Create a nodepool user
 sudo adduser --system \
-    --home $NODEPOOL_HOME \
+    --home $NODEPOOL_HOME_DIR \
     --disabled-password \
     --quiet \
     --gecos $NODEPOOL_USER \
@@ -38,30 +38,30 @@ function as_nodepool() {
 # Get nodepool sources
 as_nodepool "git clone \
     $NODEPOOL_REPO --branch $NODEPOOL_BRANCH \
-    $NODEPOOL_SRC"
+    $NODEPOOL_SRC_DIR"
 
 
 ######
 # Create virtual environment
-as_nodepool "virtualenv $NODEPOOL_VENV"
+as_nodepool "virtualenv $NODEPOOL_VENV_DIR"
 
 function in_venv() {
-    as_nodepool "set +u && . $NODEPOOL_VENV/bin/activate && set -u && $1"
+    as_nodepool "set +u && . $NODEPOOL_VENV_DIR/bin/activate && set -u && $1"
 }
 
 
 ######
 # Install python requirements
 in_venv "pip install -U distribute"
-in_venv "cd $NODEPOOL_SRC && pip install -U -r requirements.txt"
-in_venv "cd $NODEPOOL_SRC && pip install ."
+in_venv "cd $NODEPOOL_SRC_DIR && pip install -U -r requirements.txt"
+in_venv "cd $NODEPOOL_SRC_DIR && pip install ."
 in_venv "pip install python-novaclient rackspace-auth-openstack"
 
 
 ######
 # Create config and logging dir
 as_nodepool "mkdir $NODEPOOL_CFG_DIR"
-as_nodepool "mkdir $NODEPOOL_LOGS"
+as_nodepool "mkdir $NODEPPOL_LOGS_DIR"
 
 
 ######
@@ -113,19 +113,19 @@ args=(sys.stdout,)
 level=DEBUG
 class=logging.handlers.TimedRotatingFileHandler
 formatter=simple
-args=('$NODEPOOL_LOGS/debug.log', 'midnight', 1, 30,)
+args=('$NODEPPOL_LOGS_DIR/debug.log', 'midnight', 1, 30,)
 
 [handler_normal]
 level=INFO
 class=logging.handlers.TimedRotatingFileHandler
 formatter=simple
-args=('$NODEPOOL_LOGS/nodepool.log', 'midnight', 1, 30,)
+args=('$NODEPPOL_LOGS_DIR/nodepool.log', 'midnight', 1, 30,)
 
 [handler_image]
 level=INFO
 class=logging.handlers.TimedRotatingFileHandler
 formatter=simple
-args=('$NODEPOOL_LOGS/image.log', 'midnight', 1, 30,)
+args=('$NODEPPOL_LOGS_DIR/image.log', 'midnight', 1, 30,)
 
 [formatter_simple]
 format=%(asctime)s %(levelname)s %(name)s: %(message)s
@@ -142,11 +142,11 @@ chdir /
 
 script
     export NODEPOOL_SSH_KEY="\$(cat $HOME/.ssh/nodepool.pub)"
-    $NODEPOOL_VENV/bin/python \\
-        $NODEPOOL_VENV/bin/nodepoold \\
+    $NODEPOOL_VENV_DIR/bin/python \\
+        $NODEPOOL_VENV_DIR/bin/nodepoold \\
         -c $NODEPOOL_CFG_DIR/nodepool.yaml \\
         -l $NODEPOOL_CFG_DIR/logging.conf \\
-        -p $NODEPOOL_HOME/nodepool.pid \\
+        -p $NODEPOOL_HOME_DIR/nodepool.pid \\
         -d
 end script
 NODEPOOLSTARTER
