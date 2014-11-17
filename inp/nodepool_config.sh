a46962784ff2d29e6aa5bb94219c09b8db263461
Mate Lakat <mate.lakat@citrix.com>
2014-11-17 16:51:11 +0100

    FIx typo

--- inp/nodepool_config.sh
+++ inp/nodepool_config.sh
@@ -105,7 +105,7 @@ chdir $NODEPOOL_HOME_DIR
 
 script
     export NODEPOOL_SSH_KEY="\$(cat $NODEPOOL_HOME_DIR/.ssh/jenkins.pub | cut -d' ' -f 2)"
-    export NODEPOOL_PYPI_REPO="http://pypi.python.org/simple"
+    export NODEPOOL_PYPI_MIRROR="http://pypi.python.org/simple"
     /opt/nodepool/env/bin/python \\
         /opt/nodepool/env/bin/nodepoold \\
         -l /etc/nodepool/logging.conf \\
@@ -134,7 +134,7 @@ sudo tee /usr/local/bin/osci-nodepool << EOF
 #!/bin/bash
 sudo -u $NODEPOOL_USER -i \\
     NODEPOOL_SSH_KEY="\$(cat $NODEPOOL_HOME_DIR/.ssh/jenkins.pub | cut -d' ' -f 2)" \\
-    NODEPOOL_PYPI_REPO="http://pypi.python.org/simple" \\
+    NODEPOOL_PYPI_MIRROR="http://pypi.python.org/simple" \\
     /opt/nodepool/env/bin/nodepool "\$@"
 EOF
 
